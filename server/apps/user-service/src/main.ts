a43305b78a8e73a99475b9465705c4754cdb61f9
adnan <adnan172203@gmail.com>
2023-11-28 05:47:32 +0600

    user-service: changed user service name name and fixed yml file

--- server/apps/user-service/src/main.ts
+++ server/apps/user-service/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
-// import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 // Load environment variables from .env file
-// dotenv.config();
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
